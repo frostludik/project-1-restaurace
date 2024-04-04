7d62b3855eb82190c7a20cf56b9eb7e8f396cbe8
Ludek Mraz <ludek.mraz@centrum.cz>
2024-04-04 14:54:17 +0200

    Created RestaurantException class

--- src/com/engeto/restaurant/util/RestaurantException.java
+++ src/com/engeto/restaurant/util/RestaurantException.java
@@ -1,2 +1,9 @@
-package com.engeto.restaurant.util;public class RestaurantException {
+package com.engeto.restaurant.util;
+
+public class RestaurantException extends Exception{
+
+    public RestaurantException(String message) {
+        super(message);
+    }
 }
+
