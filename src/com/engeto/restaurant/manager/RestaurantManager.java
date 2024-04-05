556efe2231bac2d74ee8b43200d8256209b52c0d
Ludek Mraz <ludek.mraz@centrum.cz>
2024-04-05 10:56:18 +0200

    Added attributes and getters/setter

--- src/com/engeto/restaurant/manager/RestaurantManager.java
+++ src/com/engeto/restaurant/manager/RestaurantManager.java
@@ -1,2 +1,32 @@
-package com.engeto.restaurant.manager;public class RestaurantManager {
+package com.engeto.restaurant.manager;
+
+import com.engeto.restaurant.model.Order;
+import com.engeto.restaurant.model.Table;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestaurantManager {
+
+    private static List<Order> ordersList = new ArrayList<>();
+    private static List<Table> tablesList = new ArrayList<>();
+
+
+
+
+    public static List<Order> getOrdersList() {
+        return ordersList;
+    }
+
+    public static void setOrdersList(List<Order> ordersList) {
+        RestaurantManager.ordersList = ordersList;
+    }
+
+    public static List<Table> getTablesList() {
+        return tablesList;
+    }
+
+    public static void setTablesList(List<Table> tablesList) {
+        RestaurantManager.tablesList = tablesList;
+    }
 }
