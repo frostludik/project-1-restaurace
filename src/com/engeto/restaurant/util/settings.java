955538637b660ed1c8942b84a780f795d5c5deb4
Ludek Mraz <ludek.mraz@centrum.cz>
2024-04-05 09:53:25 +0200

    Added file paths to settings

--- src/com/engeto/restaurant/util/settings.java
+++ src/com/engeto/restaurant/util/settings.java
@@ -1,2 +1,13 @@
-package com.engeto.restaurant.util;public class settings {
+package com.engeto.restaurant.util;
+
+public class settings {
+    private static final String COOKBOOK_FILE = "resources/cookbook.txt";
+    private static final String ORDERS_FILE = "resources/orders.txt";
+
+    public static String getCookbookFile() {
+        return COOKBOOK_FILE;
+    }
+    public static String getOrdersFile() {
+        return ORDERS_FILE;
+    }
 }
