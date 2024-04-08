9c71b50cdc483f23c216bb1403f52dc3bd26385f
Ludek Mraz <ludek.mraz@centrum.cz>
2024-04-08 15:24:17 +0200

    Import method moved to RestaurantManager

--- src/com/engeto/restaurant/model/DishList.java
+++ src/com/engeto/restaurant/model/DishList.java
@@ -1,12 +1,10 @@
 package com.engeto.restaurant.model;
 
 import com.engeto.restaurant.util.RestaurantException;
-import com.engeto.restaurant.util.Settings;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class DishList {
 
@@ -26,27 +24,6 @@ public class DishList {
 
     public void removeDish(Dish dish) {
         dishes.remove(dish);
-    }
-
-
-    public void exportToFile(String fileName) throws RestaurantException {
-        try (PrintWriter outputWriter = new PrintWriter(new BufferedWriter(new FileWriter(fileName)))) {
-            for (Dish dish : dishes) {
-                outputWriter.println(dish.exportToString());
-            }
-        } catch (IOException e) {
-            throw new RestaurantException("Nepodařilo se nahrát data do souboru: " + fileName);
-        }
-    }
-    public void importFromFile() throws RestaurantException {
-        String fileName = Settings.getCookbookFile();
-        try (Scanner scanner = new Scanner(new BufferedReader(new FileReader(fileName)))) {
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                dishes.add(Dish.parseDish(line)); }
-        } catch (FileNotFoundException e) {
-            throw new RestaurantException("File" + fileName + " not found!");
-        }
     }
 
 }
