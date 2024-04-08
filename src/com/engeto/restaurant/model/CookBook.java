95b94d3e82e6cb8a887817fa3a41db300feb2901
Ludek Mraz <ludek.mraz@centrum.cz>
2024-04-08 10:11:23 +0200

    Prepared Class and its getters/setters

--- src/com/engeto/restaurant/model/CookBook.java
+++ src/com/engeto/restaurant/model/CookBook.java
@@ -1,2 +1,26 @@
-package com.engeto.restaurant.model;public class CookBook {
+package com.engeto.restaurant.model;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Collection;
+import java.util.Optional;
+
+public class CookBook {
+
+    private Map<Long, Dish> dishes = new HashMap<>();
+
+    public void addDish(Dish dish) {
+        dishes.put(dish.getId(), dish);
+    }
+
+    public void removeDishById(long id) {
+        dishes.remove(id);
+    }
+
+    public Optional<Dish> getDishById(long id) {
+        return Optional.ofNullable(dishes.get(id));
+    }
+
+    public Collection<Dish> getAllDishes() {
+        return dishes.values();
+    }
 }
