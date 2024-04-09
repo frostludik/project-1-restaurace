72a615f9fe2bb10fc2a5c678db1e766b1094c4a7
Ludek Mraz <ludek.mraz@centrum.cz>
2024-04-09 13:12:42 +0200

    changes in constructor

--- src/com/engeto/restaurant/model/Table.java
+++ src/com/engeto/restaurant/model/Table.java
@@ -5,20 +5,20 @@ import com.engeto.restaurant.util.RestaurantException;
 import java.util.List;
 
 public class Table {
-    private int tableNumber;
+    private static int tableNumber;
 
     public Table(int tableNumber) {
-        this.tableNumber = tableNumber;
+        Table.tableNumber = tableNumber;
         RestaurantManager.addTable(this);
     }
 
-    public int getTableNumber() {
+    public static int getTableNumber() {
         return tableNumber;
     }
 
     public static Table getTableByNumber(int tableNumber, List<Table> tables) throws RestaurantException {
         for (Table table : tables) {
-            if (table.getTableNumber() == tableNumber) {
+            if (getTableNumber() == tableNumber) {
                 return table;
             }
         }
