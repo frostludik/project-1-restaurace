da927a5dc6d39e9951cd5b09db33c99a456fa4b8
Ludek Mraz <ludek.mraz@centrum.cz>
2024-04-09 12:34:57 +0200

    Fixed path issues

--- src/com/engeto/restaurant/util/Settings.java
+++ src/com/engeto/restaurant/util/Settings.java
@@ -4,10 +4,8 @@ public class Settings {
     private static final String COOKBOOK_FILE = "resources/cookbook.txt";
     private static final String ORDERS_FILE = "resources/orders.txt";
 
-    public static String getCookbookFile() {
-        return COOKBOOK_FILE;
-    }
-    public static String getOrdersFile() {
-        return ORDERS_FILE;
-    }
+    public static String getCookbookFile() {return COOKBOOK_FILE;}
+    public static String getOrdersFile() {return ORDERS_FILE;}
+
+
 }
