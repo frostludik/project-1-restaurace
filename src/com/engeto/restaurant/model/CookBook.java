5c21f137e70e2921ad1503f42b7e7522429ee4af
Ludek Mraz <ludek.mraz@centrum.cz>
2024-04-10 15:15:53 +0200

    Changed mapping ID to Long and rebuilt methods
                       }

--- src/com/engeto/restaurant/model/CookBook.java
+++ src/com/engeto/restaurant/model/CookBook.java
@@ -1,34 +1,37 @@
 package com.engeto.restaurant.model;
 import com.engeto.restaurant.util.RestaurantException;
 
-import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class CookBook {
 
-    private static Map<String, Dish> dishMap = new LinkedHashMap<>();
+    private static Map<Long, Dish> dishMap = new LinkedHashMap<>();
 
     public static void addDishToCookBook(Dish dish) {
-        dishMap.put(dish.getTitle(), dish);
+        dishMap.put(dish.getId(), dish);
     }
 
-    public void removeDishById(long dishId) throws RestaurantException {
-        try {
-            Dish dishToRemove = getDishById(dishId);
-            dishMap.remove(dishToRemove.getTitle());
-            System.out.println("Pokrm č. " + dishId + " - " + dishToRemove.getTitle() + " byl úspěšně odstraněn.");
-        } catch (RestaurantException e) {
-            throw new RestaurantException("Chyba při pokusu o odstranění pokrmu: " + e.getMessage());
+    public static void removeDishById(long dishId) throws RestaurantException {
+        if (dishMap.containsKey(dishId)) {
+            Dish dishToRemove = dishMap.remove(dishId);
+            System.out.println("Dish nr. " + dishId + " - " + dishToRemove.getTitle() + " was successfully removed!");
+        } else {
+            throw new RestaurantException("Dish with ID " + dishId + " not found, cannot remove.");
         }
     }
-    public static Dish getDishById(long dishId) throws RestaurantException {
-        for (Dish dish : dishMap.values()) {
-            if (dish.getId() == dishId) {
-                return dish;
-            }
+
+    public static Dish getDishFromCookBookById(long dishId) throws RestaurantException {
+        Dish dish = dishMap.get(dishId);
+        if (dish != null) {
+            return dish;
+        } else {
+            throw new RestaurantException("Dish with ID " + dishId + " not found!");
         }
-        throw new RestaurantException("Dish with ID " + dishId + " not found!");
     }
-    public Collection<Dish> getAllDishes() {
-        return dishMap.values();
+
+    public static Map<Long, Dish> getAllDishes() {
+        return new LinkedHashMap<>(dishMap); // Return a copy to prevent modification
     }
 }
+
